c801a2b72cb519e8658e01fa0de404c675d86314
KSA01 <Kyle.ambrecht@yahoo.com>
2017-10-09 09:55:21 -0700

    Add folder file

---
+++ lakeside/__resource.lua
@@ -0,0 +1,40 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/track.ityp'
+
+files {
+    'stream/cliff.ydr',
+    'stream/cliff0.ydr',
+    'stream/cliff1.ydr',
+    'stream/cliff2.ydr',
+    'stream/grass2.ydr',
+	'stream/KERB.ydr',
+	'stream/lakeside.ybn',
+	'stream/lakeside.ytyp',
+	'stream/POST.ydr',
+	'stream/POST0.ydr',
+	'stream/POST1.ydr',
+	'stream/radar.ydr',
+	'stream/ROAD.ydr',
+	'stream/VEGETATION.ydr',
+	'stream/VEGETATION0.ydr',
+	'stream/VEGETATION1.ydr',
+	'stream/VEGETATION2.ydr',
+	'stream/VEGETATION3.ydr',
+	'stream/VEGETATION4.ydr',
+	'stream/VEGETATION5.ydr',
+	'stream/VEGETATION6.ydr',
+	'stream/VEGETATION7.ydr',
+	'stream/VEGETATION8.ydr',
+	'stream/VEGETATION9.ydr',
+	'stream/VEGETATION10.ydr',
+	'stream/WALL.ydr',
+	'stream/WALL0.ydr',
+	'stream/WALL1.ydr',
+	'stream/WALL2.ydr',
+	'stream/WALL3.ydr',
+	'stream/WALL4.ydr',
+	'stream/WALL5.ydr',
+	'stream/WALL6.ydr'
+}
